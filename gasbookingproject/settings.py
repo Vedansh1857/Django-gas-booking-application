58c3ecb446f8985997fd23c9e19721ae4ee2d068
Vedansh-aviara <vedansh.gupta@aviaralabs.com>
2024-11-14 19:17:27 +0530

    db changed again

--- gasbookingproject/settings.py
+++ gasbookingproject/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'gasbookingproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
